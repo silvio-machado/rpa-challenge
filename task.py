52fd1609ed9bf3d9878fe9f822fdbe8d9b6ac2d2
Silvio <machado.vrs@gmail.com>
2023-05-22 20:11:10 -0300

    Update task.py

--- task.py
+++ task.py
@@ -9,6 +9,7 @@ def open_the_website(url):
 
 def search_for(term):
     input_field = "css:input"
+    browser_lib.wait_until_page_contains_element(input_field)
     browser_lib.input_text(input_field, term)
     browser_lib.press_keys(input_field, "ENTER")
 
@@ -17,11 +18,31 @@ def store_screenshot(filename):
     browser_lib.screenshot(filename=filename)
 
 
+def expand_sandwich_menu():
+    browser_lib.wait_until_page_contains_element("css:#app > div:nth-child(3) > div > header > section.css-9kr9i3.e1m0pzr42 > div.css-qo6pn.ea180rp0 > div.css-6n7j50 > button")
+    browser_lib.click_element("css:#app > div:nth-child(3) > div > header > section.css-9kr9i3.e1m0pzr42 > div.css-qo6pn.ea180rp0 > div.css-6n7j50 > button")
+
+
+def define_news_range(date):
+    start_date = "05/01/2023"
+    browser_lib.wait_until_page_contains_element("css:#site-content > div.css-1wa7u5r > div.css-1npexfx > div.css-1az02az > div > div > div.css-wsup08 > div > div > button")
+    browser_lib.click_button("css:#site-content > div.css-1wa7u5r > div.css-1npexfx > div.css-1az02az > div > div > div.css-wsup08 > div > div > button")
+    browser_lib.wait_until_page_contains_element("css:#site-content > div.css-1wa7u5r > div.css-1npexfx > div.css-1az02az > div > div > div.css-wsup08 > div > div > div > ul > li:nth-child(6) > button")
+    browser_lib.click_button("css:#site-content > div.css-1wa7u5r > div.css-1npexfx > div.css-1az02az > div > div > div.css-wsup08 > div > div > div > ul > li:nth-child(6) > button")
+    start_range_input = "css:#startDate"
+    browser_lib.wait_until_page_contains_element(start_range_input)
+    browser_lib.input_text(start_range_input, "05/01/2023")
+
+def click_search_button():
+    browser_lib.click_button('xpath://*[@id="app"]/div[2]/div/header/section[1]/div[1]/div[2]/button')
+
+
 # Define a main() function that calls the other functions in order:
 def main():
     try:
-        open_the_website("https://robocorp.com/docs/")
-        search_for("python")
+        open_the_website("nytimes.com")
+        expand_sandwich_menu()
+        search_for("Brazil")
         store_screenshot("output/screenshot.png")
     finally:
         browser_lib.close_all_browsers()
